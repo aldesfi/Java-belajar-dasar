6b3aecec444657671ad81e5689159c8780470480
Aldespi Arifin <alldes182@gmail.com>
2021-02-23 14:34:18 +0700

    memahami super dan extends di java
    memahami super dan extends di java

---
+++ gunanya_super/KelasPakaiSuper.java
@@ -0,0 +1,14 @@
+class KelasPakaiSuper extends YangDiSuperin
+{
+    KelasPakaiSuper()
+    {
+       super();
+    }
+
+    public static void main(String[] args) 
+    {
+        KelasPakaiSuper KelasPakaiSuperDiObjekin = new KelasPakaiSuper();
+        KelasPakaiSuperDiObjekin.OperasiYangNggadaDisini();
+    }
+
+}
---
+++ gunanya_super/YangDiSuperin.java
@@ -0,0 +1,7 @@
+class YangDiSuperin 
+{
+    void OperasiYangNggadaDisini()
+    {
+
+    }
+}
