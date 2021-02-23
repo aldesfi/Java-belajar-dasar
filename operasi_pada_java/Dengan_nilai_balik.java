7932a2f6abc1702efdaa94b8bd0e17e5181d7773
Aldespi Arifin <alldes182@gmail.com>
2021-02-23 13:55:59 +0700

    Dengan_nilai_balik

---
+++ operasi_pada_java/Dengan_nilai_balik.java
@@ -0,0 +1,28 @@
+class Dengan_nilai_balik 
+{
+    
+    void operasi_satu()
+    {
+        
+    }
+
+    String operasi_dua()
+    {
+     String kembalikan_string;
+     return kembalikan_string;   
+    }
+
+    Integer operasi_tiga()
+    {
+     Integer kembalikan_Integer;
+     return kembalikan_Integer;   
+    }
+
+    Dengan_nilai_balik operasi_empat()
+    {
+     Dengan_nilai_balik kembalikan_Dengan_nilai_balik;
+     return kembalikan_Dengan_nilai_balik;   
+    }
+
+    
+}
